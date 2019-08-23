d876f1991a5e76fb6a90ef537e62d6ce1fe8a7f9
RawzaDigital <rawzadigital@gmail.com>
2019-08-24 02:48:40 +0700

    adding new 3 class java for presenting MVP design pattern

---
+++ app/src/main/java/com/example/rdstudio/myunittesting/MainPresenter.java
@@ -0,0 +1,19 @@
+package com.example.rdstudio.myunittesting;
+
+public class MainPresenter {
+    private MainView view;
+
+    public MainPresenter(MainView view) {
+        this.view = view;
+    }
+
+    public double volume(double length, double width, double height){
+        return length / width * height;
+    }
+
+    public void calculateVolume(double length, double width, double height){
+        double volume = volume(length, width, height);
+        MainModel model = new MainModel(volume);
+        view.showVolume(model);
+    }
+}
---
+++ app/src/main/java/com/example/rdstudio/myunittesting/MainView.java
@@ -0,0 +1,5 @@
+package com.example.rdstudio.myunittesting;
+
+public interface MainView {
+    void showVolume(MainModel model);
+}
