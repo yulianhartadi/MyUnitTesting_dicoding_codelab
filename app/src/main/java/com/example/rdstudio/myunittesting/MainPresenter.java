6bd207a17724cde6504b39624547c21d38b0fda4
RawzaDigital <rawzadigital@gmail.com>
2019-08-24 03:11:08 +0700

    volume = panjang * lebar * tinggi? seharusnya

--- app/src/main/java/com/example/rdstudio/myunittesting/MainPresenter.java
+++ app/src/main/java/com/example/rdstudio/myunittesting/MainPresenter.java
@@ -8,7 +8,7 @@ public class MainPresenter {
     }
 
     public double volume(double length, double width, double height){
-        return length / width * height;
+        return length * width * height;
     }
 
     public void calculateVolume(double length, double width, double height){
